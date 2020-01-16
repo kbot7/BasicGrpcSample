989a0c06b520ccbc2b337ca870d11a8f2a5c6321
Kevin Gysberg <kegysber@microsoft.com>
2020-01-16 08:13:25 -0600

    Updated csproj of framework sample

--- BasicGrpcSample.FrameworkClient/Program.cs
+++ BasicGrpcSample.FrameworkClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grpc.Core;
 
 namespace BasicGrpcSample.FrameworkClient
 {
@@ -10,6 +11,17 @@ namespace BasicGrpcSample.FrameworkClient
     {
         static void Main(string[] args)
         {
+            Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+            var client = new Greeter.GreeterClient(channel);
+            String user = "you";
+
+            var reply = client.SayHello(new HelloRequest { Name = user });
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
