127099d6dca031f185a6ef480bde8ffb80b8ddbd
Kevin Gysberg <kegysber@microsoft.com>
2020-01-29 12:23:43 -0600

    Cleaned up sample

--- BasicGrpcSample.ConsoleClient/Program.cs
+++ BasicGrpcSample.ConsoleClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using BasicGrpcSample.Contracts;
+﻿using BasicGrpcSample.ClientLib;
+using BasicGrpcSample.Contracts;
 using Grpc.Core;
 using Grpc.Net.Client;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,49 +16,28 @@ namespace BasicGrpcSample.ConsoleClient
 	{
 		static async Task Main(string[] args)
 		{
-			// Needed for dev env when TLS is not used
+			// Required for local development with TLS disabled
 			AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
 
-			var serviceCollection = new ServiceCollection();
-			//ConfigureServices(serviceCollection);
-			serviceCollection.AddLogging(configure => configure.AddConsole());
-			var serviceProvider = serviceCollection.BuildServiceProvider();
-
-			// The port number(5001) must match the port of the gRPC server.
-			//using var channel = GrpcChannel.ForAddress("http://localhost:5001", new GrpcChannelOptions()
-			//{
-			//	Credentials = ChannelCredentials.Insecure,
-			//	LoggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>()
-			//});
-
-			//using var channel = GrpcChannel.ForAddress("http://52.252.172.110:80", new GrpcChannelOptions()
-			//{
-			//	Credentials = ChannelCredentials.Insecure,
-			//	LoggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>()
-			//});
-
-			using var channel = GrpcChannel.ForAddress("http://grpc.jonhussdev.com", new GrpcChannelOptions()
-			{
-				Credentials = ChannelCredentials.Insecure,
-				LoggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>()
-			});
+			IGrpcClientFactory grpcFactory = new GrpcCoreClientFactory("http://localhost:5001");
+			//IGrpcClientFactory grpcFactory = new GrpcCoreClientFactory("http://grpc.jonhussdev.com");
 
 			// Contract-First - Greeter
-			var client = new Server.Greeter.GreeterClient(channel);
-			var reply = await client.SayHelloAsync(
-							  new Server.HelloRequest { Name = "GreeterClient" });
-			Console.WriteLine("Greeting: " + reply.Message);
+			//var greeter = grpcFactory.GetClient<Server.Greeter.GreeterClient>();
+			//var reply = await greeter.SayHelloAsync(
+			//				  new Server.HelloRequest { Name = "GreeterClient" });
+			//Console.WriteLine("Greeting: " + reply.Message);
 
 
 			// Code-First - Calculator
-			//var calculator = channel.CreateGrpcService<ICalculator>();
+			//var calculator = grpcFactory.GetClient<ICalculator>();
 			//var result = await calculator.MultiplyAsync(new MultiplyRequest { X = 12, Y = 4 });
 			//Console.WriteLine(result.Result);
 
 			// Code-First - Order Service
-			//var orderSvc = channel.CreateGrpcService<IOrderService>();
-			//var result = await orderSvc.GetOrdersAsync(new GetOrdersRequest { OrderIds = new List<string> { "1", "2" } });
-			//Console.WriteLine(JsonConvert.SerializeObject(result));
+			var orderSvc = grpcFactory.GetClient<IOrderService>();
+			var result = await orderSvc.CreateOrderAsync(new CreateOrderRequest() { CustomerId = "123", DateTimePlaced = DateTime.UtcNow, Skus = new List<string> { "a", "b" } });
+			Console.WriteLine(JsonConvert.SerializeObject(result));
 
 			Console.WriteLine("Press any key to exit...");
 			Console.ReadKey();
--- BasicGrpcSample.FrameworkClient/Program.cs
+++ BasicGrpcSample.FrameworkClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BasicGrpcSample.ClientLib;
 using BasicGrpcSample.Contracts;
 using BasicGrpcSample.Server;
 using Grpc.Core;
@@ -14,21 +15,19 @@ namespace BasicGrpcSample.FrameworkClient
     {
         static async Task Main(string[] args)
         {
-            Channel channel = new Channel("127.0.0.1:5001", ChannelCredentials.Insecure);
+            IGrpcClientFactory grpcFactory = new GrpcFrameworkClientFactory("localhost:5001");
 
-            //// Contract-First Greeter
-            //var client = new Greeter.GreeterClient(channel);
-            //String user = "you";
-            //var reply = client.SayHello(new HelloRequest { Name = user });
-            //Console.WriteLine("Greeting: " + reply.Message);
+            // Contract-First - Greeter
+            var client = grpcFactory.GetClient<Greeter.GreeterClient>();
+            String user = "Kevin";
+            var reply = client.SayHello(new HelloRequest { Name = user });
+            Console.WriteLine("Greeting: " + reply.Message);
 
-            // Code-First Calculator
-            var calculator = channel.CreateGrpcService<ICalculator>();
+            // Code-First - Calculator
+            var calculator = grpcFactory.GetClient<ICalculator>();
             var result = await calculator.MultiplyAsync(new MultiplyRequest { X = 12, Y = 4 });
             Console.WriteLine(result.Result);
 
-
-            channel.ShutdownAsync().Wait();
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
